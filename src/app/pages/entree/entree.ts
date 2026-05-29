b1bb82a9e347bf4cb22932c5db9544b23f215dbd
leanledivin <niangne_lowes@divingrace.com>
2026-05-29 20:42:54 +0000

    gestion des entrées

--- src/app/pages/entree/entree.ts
+++ src/app/pages/entree/entree.ts
@@ -3,107 +3,168 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { ServiceEntree } from '../../service/service-entree';
 
-//pour la gestion des alertes
+// gestion des alertes
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-entree',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './entree.html',
   styleUrl: './entree.css',
 })
 export class Entree implements OnInit {
 
-  eqpt:any
-  eq:any[]=[];
-  sit:any[]=[];
+  eqpt: any;
+  eq: any[] = [];
+  sit: any[] = [];
 
-   message = '';
-showMessage = false;
+  // fichier sélectionné
+  selectedFile!: File;
+
+  message = '';
+  showMessage = false;
 
   formData = {
     dateEntree: '',
+    modele: '',
     id_sit: null,
     id_eqpt: null,
     codeSite: '',
     numeroSerie: '',
     motif: '',
-    traitement: '',
     dateDebut: '',
-    dateFin: '',
     statut: '',
-    fichier:''
+    fichier: ''
   };
 
+  // injection du service
+  constructor(private entreeservice: ServiceEntree) {}
 
-  // injection du service 
-  constructor(private entreeservice:ServiceEntree){}
-  
-  ngOnInit(){
+  ngOnInit() {
     this.combogeteqpt();
     this.combogetsitFentrees();
   }
 
+  // récupération du fichier
+  onFileSelected(event: any) {
+
+    if (event.target.files.length > 0) {
+
+      this.selectedFile = event.target.files[0];
+
+    }
+  }
+
   // cliquer sur enregistrer
   onSubmit() {
-    // ✅ message succès
-     
-          Swal.fire({
-                  icon: 'success',
-                  title: 'Succès',
-                  text: 'Entrée enregistré avec succès !',
-                  timer: 2000,
-                  showConfirmButton: false
-                });
-                
-        // 2. Réinitialiser le formulaire
-              this.formData = {
-                dateEntree: '',
-                id_sit: null,
-                id_eqpt: null,
-                codeSite: '',
-                numeroSerie: '',
-                motif: '',
-                traitement: '',
-                dateDebut: '',
-                dateFin: '',
-                statut: '',
-                fichier:''
-              };
-        }
+
+    this.insertentreedata();
+
+  }
 
   onReset(form: NgForm) {
     form.resetForm();
   }
 
+  // récupération des entrées
+  getentreedata() {
 
-  //recuperation des entrée
-  getentreedata(){
-        this.entreeservice.getEntree().subscribe(res =>{
-        this.eqpt = res;
-      });
-    }
+    this.entreeservice.getEntree().subscribe(res => {
 
-    //récupération des sites dans combo site duForm des entrées
-    combogetsitFentrees(){
-      this.entreeservice.getSiteComboFentree().subscribe(res =>{
-        this.sit = res;
-      })
-    }
+      this.eqpt = res;
 
-    //récupération des equipements
-    combogeteqpt(){
-      this.entreeservice.getEqptCombo().subscribe(res =>{
-        this.eq = res
-      })
-    }
+    });
 
-    //insertion des données
-    insertentreedata(){
-      this.entreeservice.insertEntree(this.formData).subscribe(res =>{
-        console.log(this.formData);  
-      })
-    }
+  }
+
+  // récupération des sites
+  combogetsitFentrees() {
+
+    this.entreeservice.getSiteComboFentree().subscribe(res => {
+
+      this.sit = res;
+
+    });
+
+  }
+
+  // récupération des équipements
+  combogeteqpt() {
+
+    this.entreeservice.getEqptCombo().subscribe(res => {
+
+      this.eq = res;
+
+    });
+
+  }
+
+  // insertion des données
+  insertentreedata() {
+
+    const data = new FormData();
+
+    // image
+    data.append('fichier', this.selectedFile);
+
+    // correspond à "model" attendu par Laravel
+    data.append('modele', this.formData.modele);
+
+    data.append('dateEntree', this.formData.dateEntree);
+    data.append('dateDebut', this.formData.dateDebut);
+    data.append('codeSite', this.formData.codeSite);
+    data.append('numeroSerie', this.formData.numeroSerie);
+    data.append('motif', this.formData.motif);
+    data.append('statut', this.formData.statut);
+
+    data.append('id_sit', String(this.formData.id_sit));
+    data.append('id_eqpt', String(this.formData.id_eqpt));
+
+    this.entreeservice.insertEntree(data).subscribe({
+
+      next: (res) => {
+
+        console.log(res);
+
+        Swal.fire({
+          icon: 'success',
+          title: 'Succès',
+          text: 'Entrée enregistrée avec succès !',
+          timer: 2000,
+          showConfirmButton: false
+        });
+
+        // réinitialisation du formulaire
+        this.formData = {
+          dateEntree: '',
+          modele: '',
+          id_sit: null,
+          id_eqpt: null,
+          codeSite: '',
+          numeroSerie: '',
+          motif: '',
+          dateDebut: '',
+          statut: '',
+          fichier: ''
+        };
+
+      },
+
+      error: (err) => {
+
+        console.log(err);
+
+        Swal.fire({
+          icon: 'error',
+          title: 'Erreur',
+          text: 'Erreur lors de l’enregistrement'
+        });
+
+      }
+
+    });
+
+  }
 
 }
