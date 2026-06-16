4fdc76d38c86a745d927f027107385c08e790486
leanledivin <niangne_lowes@divingrace.com>
2026-06-16 15:41:39 +0000

    login

--- src/app/authent/login/login.ts
+++ src/app/authent/login/login.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone:true,
-  imports: [RouterLink, RouterModule],
+  imports: [RouterLink, RouterModule, CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+
+
+}
