456750da5e62c869ced704195b9b55b79fcdc52f
malachie45 <jeanledivinlowes@gmail.com>
2026-06-25 16:28:31 +0000

    dashboard actualisé

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,21 +1,81 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';  // nécessaire pour *ngFor
+import { CommonModule } from '@angular/common';
 import { Statistiques } from '../../service/statistiques';
+import { BaseChartDirective } from 'ng2-charts';
 
+import {
+  Chart,
+  PieController,
+  ArcElement,
+  Tooltip,
+  Legend,
+  BarController,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js';
 
+Chart.register(
+  PieController,
+  ArcElement,
+  Tooltip,
+  Legend,
+  BarController,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BaseChartDirective],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard implements OnInit {
-  
- 
 
-  constructor(private statservice:Statistiques) { }
+  // -----------------------------
+  // PIE CHART
+  // -----------------------------
+  pieChartLabels: string[] = [
+    'Entrées',
+    'Sorties',
+    'Missions'
+  ];
+
+  pieChartData = [
+    {
+      data: [0, 0, 0],
+      backgroundColor: [
+        '#0d6efd', // bleu
+        '#198754', // vert
+        '#ffc107'  // jaune
+      ]
+    }
+  ];
+
+  // -----------------------------
+  // BAR CHART
+  // -----------------------------
+  barChartLabels: string[] = [];
+
+  barChartData = [
+    {
+      data: [] as number[],
+      label: 'Entrées',
+      backgroundColor: '#0d6efd'
+    },
+    {
+      data: [] as number[],
+      label: 'Sorties',
+      backgroundColor: '#198754'
+    }
+  ];
+
+  constructor(
+    private statservice: Statistiques
+  ) {}
 
   statss: any = {};
   statssbyeqpt: any[] = [];
@@ -25,20 +85,61 @@ export class Dashboard implements OnInit {
     this.getstatisbyeqpt();
   }
 
- 
+  // -----------------------------
+  // STATISTIQUES GLOBALES
+  // -----------------------------
+  getstatis() {
 
-  //conexion au service stat
-  getstatis(){
-        this.statservice.getstatistiq().subscribe(res =>{
-        this.statss = res;
-      });
-    }
+    this.statservice.getstatistiq().subscribe((res: any) => {
 
-    //gestion du tableau des stat
-    getstatisbyeqpt(){
-      this.statservice.getstatpareqpt().subscribe(res =>{
-        this.statssbyeqpt = res;
-      });
-    }
+      this.statss = res;
+
+      this.pieChartData = [
+        {
+          data: [
+            res.nombre_entrees,
+            res.nombre_sorties,
+            res.nombre_missions
+          ],
+          backgroundColor: [
+            '#0d6efd',
+            '#198754',
+            '#ffc107'
+          ]
+        }
+      ];
+
+    });
+
+  }
+
+  // -----------------------------
+  // STATISTIQUES PAR EQUIPEMENT
+  // -----------------------------
+  getstatisbyeqpt() {
+
+    this.statservice.getstatpareqpt().subscribe((res: any[]) => {
+
+      this.statssbyeqpt = res;
+
+      this.barChartLabels =
+        res.map(item => item.nom_eqpt);
+
+      this.barChartData = [
+        {
+          data: res.map(item => item.entrees),
+          label: 'Entrées',
+          backgroundColor: '#0d6efd'
+        },
+        {
+          data: res.map(item => item.sorties),
+          label: 'Sorties',
+          backgroundColor: '#198754'
+        }
+      ];
+
+    });
+
+  }
 
 }
