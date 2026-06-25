6a173375c93c88d80a7f3c38fafcb12f0890850b
malachie45 <jeanledivinlowes@gmail.com>
2026-06-25 09:28:01 +0000

    CLEAN OK

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -3,12 +3,7 @@ import { CommonModule } from '@angular/common';  // nécessaire pour *ngFor
 import { Statistiques } from '../../service/statistiques';
 
 
-interface EquipementStats {
-  nom: string;
-  entrees: number;
-  sorties: number;
-  missions: number;
-}
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -18,17 +13,7 @@ interface EquipementStats {
 })
 export class Dashboard implements OnInit {
   
-  stats: {
-    entrees: number;
-    sorties: number;
-    missions: number;
-    equipements: EquipementStats[];
-  } = {
-    entrees: 0,
-    sorties: 0,
-    missions: 0,
-    equipements: []
-  };
+ 
 
   constructor(private statservice:Statistiques) { }
 
@@ -36,24 +21,11 @@ export class Dashboard implements OnInit {
   statssbyeqpt: any[] = [];
 
   ngOnInit(): void {
-    this.loadStats();
     this.getstatis();
     this.getstatisbyeqpt();
   }
 
-  loadStats(): void {
-    // Données statiques pour le test, à remplacer par un service API
-    this.stats = {
-      entrees: 150,
-      sorties: 120,
-      missions: 35,
-      equipements: [
-        { nom: 'Equipement A', entrees: 50, sorties: 40, missions: 10 },
-        { nom: 'Equipement B', entrees: 70, sorties: 60, missions: 15 },
-        { nom: 'Equipement C', entrees: 30, sorties: 20, missions: 10 },
-      ]
-    };
-  }
+ 
 
   //conexion au service stat
   getstatis(){
